b883461b132bea3c1c7e31559a3f21c19bd3d7d1
Yashraj950 <yashkumar9ipo@gmail.com>
2024-09-13 21:52:33 +0530

    New update

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
 import './style.css'
+
+
+import products from "./api/products.json";
+import { showProductContainer } from './api/homeProductCards';
+
+
+//  Define a function name  `showProductContainer` that takes a an array of products as input.
+
+showProductContainer(products);
