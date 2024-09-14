62933a6d6330f62ded7d9d3cb138cfb2a980fcde
Yashraj950 <yashkumar9ipo@gmail.com>
2024-09-14 10:02:45 +0530

    Update

--- api/homeProductCards.js
+++ api/homeProductCards.js
@@ -1,22 +1,39 @@
-const productContainer = document.querySelector("#productContainer")
-const productTemplate = document.querySelector("#productTemplate")
+import { homeQuantityToggle } from "./homeQuantityToggle";
+
+const productContainer = document.querySelector("#productContainer");
+const productTemplate = document.querySelector("#productTemplate");
 export const showProductContainer = (products) => {
+  if (!products) {
+    return false;
+  }
+
+  products.forEach((curProd) => {
+    const { brand, category, description, id, image, name, price, stock } =
+      curProd;
+
+    const productClone = document.importNode(productTemplate.content, true);
+
+    productClone.querySelector("#cardValue").setAttribute("id", `card${id}`);
 
 
-    if(!products) {
-        return false;
-    }
+    productClone.querySelector(".category").textContent = category;
 
-    products.forEach((curProd)=>{
-        const { brand, category, description, id,image, name, price, stock} = curProd;
+    productClone.querySelector(".productName").textContent = name;
+    productClone.querySelector(".productImage").src = image;
+    productClone.querySelector(".productImage").alt = name;
+    productClone.querySelector(".productStock").textContent = stock;
+    productClone.querySelector(".productDescription").textContent = description;
+    productClone.querySelector(".productPrice").textContent = `₹${price}`;
+    productClone.querySelector(".productActualPrice").textContent = `₹${
+      price * 2
+    }`;
 
-        const productClone = document.importNode(productTemplate.content, true)
+    productClone
+      .querySelector(".stockElement")
+      .addEventListener("click", (event) => {
+        homeQuantityToggle(event, id, stock);
+      });
 
-        productClone.querySelector(".productName").textContent = name;
-        productClone.querySelector('.productImage').src = image;
-        productClone.querySelector('.productImage').alt = name;
-
-        productContainer.append(productClone);
-    })
-
-}
+    productContainer.append(productClone);
+  });
+};
